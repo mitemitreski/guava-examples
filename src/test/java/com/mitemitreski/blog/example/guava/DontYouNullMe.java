f77e587f7e3d02bd76bcc0fc5219f0ae3311f2a5
Mite Mitreski <mmitresk@fs1.netcetera.com.mk>
2012-02-07 19:35:55 +0100

    added otheer examples and better explanation

--- src/test/java/com/mitemitreski/blog/example/guava/DontYouNullMe.java
+++ src/test/java/com/mitemitreski/blog/example/guava/DontYouNullMe.java
@@ -1,11 +1,16 @@
 package com.mitemitreski.blog.example.guava;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import com.google.common.base.Objects;
 import com.google.common.base.Optional;
 import com.google.common.base.Strings;
 import com.mitemitreski.blog.example.bean.Customer;
+
+import static org.junit.Assert.assertTrue;
+
+import static org.junit.Assert.assertNotNull;
 
 import static org.junit.Assert.assertEquals;
 
@@ -19,41 +24,44 @@ public class DontYouNullMe {
   @Test
   public void optionalExample() {
     Optional<Integer> possible = Optional.of(3);// Make optional of given type
-    possible.isPresent(); // returns true if nonNull
-    possible.or(10); // returns this possible value or default
-    possible.get(); // returns 3
+    possible.isPresent(); // evaluate to true if nonNull
+    possible.or(10); // evaluate to possible's value or default
+    possible.get(); // evaluate to 3
   }
 
   @Test
   public void firstNotNull() {
-    Integer a = Objects.firstNonNull(null, 3);// will return 3
-    Integer b = Objects.firstNonNull(9, 3);// //will return 9
-    System.out.println(a + " " + b);
+    Integer a = Objects.firstNonNull(null, 3);// will evaluate to 3
+    Integer b = Objects.firstNonNull(9, 3);// //will evaluate to 9
+    assertEquals(Integer.valueOf(3), a);
+    assertEquals(Integer.valueOf(9), b);
   }
 
-  @Test
-  public void nullInStrings() {
-    String res = Strings.emptyToNull("");
-    System.out.println(res);
-    res = Strings.nullToEmpty(res);
-    System.out.println(res);
-    boolean isIt = Strings.isNullOrEmpty(res);
-    System.out.println(isIt);
-  }
-
-  // TODO add obj test
 
   @Test
   public void testNeverNullWithoutGuava() {
     Integer defaultId = null;
     Integer id = theUnknowMan.getId() != null ? theUnknowMan.getId() : defaultId;
+    assertNotNull(id);
   }
 
   @Test(expected = NullPointerException.class)
   public void testNeverNullWithGuava() {
     Integer defaultId = null;
     int id = Objects.firstNonNull(theUnknowMan.getId(), defaultId);
-    assertEquals(0, id);
+    assertNotNull(id);
+  }
 
+  @Test
+  public void nullInStrings() {
+    String res = Strings.emptyToNull("");// res will be null
+    res = Strings.nullToEmpty(res);// res will be ""
+    boolean isIt = false;
+    if (res == null || "".equals(res)) {
+      isIt = true;
+    }
+    boolean isItWithG = Strings.isNullOrEmpty(res);
+    assertTrue(isIt);
+    assertTrue(isItWithG);
   }
 }
---
+++ src/test/java/com/mitemitreski/blog/example/guava/PreconditionsExample.java
@@ -0,0 +1,19 @@
+package com.mitemitreski.blog.example.guava;
+
+import org.junit.Test;
+
+import static com.google.common.base.Preconditions.*;
+import com.mitemitreski.blog.example.bean.Customer;
+
+public class PreconditionsExample {
+
+  private Customer theUnknowMan = new Customer();
+
+  @Test(expected = IllegalArgumentException.class)
+  public void somePreconditions() {
+    checkNotNull(theUnknowMan.getId()); // Will throw NPE
+    checkState(!theUnknowMan.isSick()); // Will throw IllegalStateException
+    checkArgument(theUnknowMan.getAddress() != null,
+        "We couldn't find the description for customer with id %s", theUnknowMan.getId());
+  }
+}
